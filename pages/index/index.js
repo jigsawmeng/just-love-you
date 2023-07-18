d93d523134729f97094c107f5373354e4cd2ad3f
221091 <mengqingjia@eastmoney.com>
2023-07-18 09:57:02 +0800

    1.0.32 1.0.32 7.21见面，日期修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,7 +11,7 @@ Page({
     nowDay: "",
     loveDay: "2023/02/05",
     loveDayDistance: "",
-    nextDay: "2023/07/28",
+    nextDay: "2023/07/21",
     nextDayRealDistance: "",
     nextDayDistance: "",
     checked: false, // 背景音乐开始暂停
